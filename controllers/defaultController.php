c344705144c2c052e17d8dd2b42430a11aa1c1fb
Weeknd05 <memecicaadrian223@gmail.com>
2023-05-03 19:09:36 +0200

    asfsa

--- controllers/defaultController.php
+++ controllers/defaultController.php
@@ -1,5 +1,6 @@
 <?php
 /* el controlador principal que dirije a los views, se encargara de llamar funciones
 */
+echo "hola";
 require("../entities/Producte.php")
 ?>
--- entities/Producte.php
+++ entities/Producte.php
@@ -10,7 +10,7 @@ function mostrarProducte() {
         foreach($result as $row) {
         echo "<div>";
         $url = $row['foto'];
-        echo "<td>".$row['nom']."</td><td>".$row['descripcio']."</td><td>".$row['preu']."</td><td>"."<img src=$url>"."</td><td>".$row['stock']."</td><td>".$row['menu']."</td>";
+        echo "<td>".$row['nom']."</td><td>".$row['descripcio']."</td><td>".$row['preu']."</td><td>"."<img src=$url>"."</td><td>".$row['stock']."</td><td>".$row['menu']."</td><td>".$row['mides']."</td>";
         echo "<div>" ;
         }
         echo "</div>";

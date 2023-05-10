b89e59ecb4ad38c063fc119479c7d302e0554eda
Cristian <crifergon1@gmail.com>
2023-05-10 19:41:31 +0200

    novaPujada

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require("./controllers/defaultController.php");
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Original Kebab House </title>
+</head>
+
+<body>
+    <!-- Falta header-->
+    <?php
+    require("./controllers/defaultController.php");
 
 
-?>
+    ?>
+</body>
+<footer>
+    <p>
+        Projecte M04 Crud amb PHP Adrian i Cristian &#169
+    </p>
+</footer>
+
+</html>
---
+++ view/viewLoginUser.php
@@ -0,0 +1,10 @@
+<div class="LoginModal">
+    <section class="cardLogin">
+        <form action="index.php" method="post">
+            NOM: <input type="text" name="name" id="name">
+            PASSWORD: <input type="password" name="password" id="password">
+            <input type="hidden" name="action" value="add">
+            <input type="submit" value="Enviar">
+        </form>
+    </section>
+</div>

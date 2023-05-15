0e3ac812a29e35ad94beffbfadc3dfae6f24beaf
Cristian <crifergon1@gmail.com>
2023-05-15 18:52:58 +0200

    sesions

--- controllers/login.php
+++ controllers/login.php
@@ -1,24 +1,27 @@
 <?php
+//rebem paramtres form
 $user = isset($_POST["name"]) ? $_POST["name"] : "Desconegut";
-$contrasenya = isset($_POST["pass"]) ? $_POST["name"] : "Desconegut";
+$contrasenya = isset($_POST["pass"]) ? $_POST["password"] : "Desconegut";
 $frase = 'AvuiFaBonsolNosabran%Dequevaaquestacontrasenya-#^';
 $missatgeDigest = hash('sha512', $contrasenya . $frase);
 $login = false;
+$rol;
 //  EN ESTA PÀGINA COMPROVAMOS SI EL USUARIO EXISTE EN NUESTRA BD I REDIRIGIMOS A HOME DE CADA UNO TMB SI NO EXISTE 
 
 while (current($users) !== FALSE) {
 
     if (key($users) == $user && $missatgeDigest == $users[key($users)]) {
         $login = true;
+        $rol = $users["rol"];
     }
     next($users);
 }
 
 if ($login) {
     $_SESSION["name"] = isset($_POST["name"]) ? $_POST["name"] : $_SESSION["name"];
-
+    $_SESSION["rol"] = $rol;
+    header("Location: index.php");
 } else {
-
     exit();
 }
 ?>
--- view/viewLoginUser.php
+++ view/viewLoginUser.php
@@ -1,6 +1,6 @@
 <div class="LoginModal">
     <section class="cardLogin">
-        <form action="login.php" method="post" onsubmit="window.location.href = 'login.php';">
+        <form action="login.php" method="post" onsubmit="window.location.href = './controllers/login';">
             NOM: <input type="text" name="name" id="name">
             PASSWORD: <input type="password" name="password" id="password">
             <input type="hidden" name="action" value="log">
